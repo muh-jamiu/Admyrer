b2d67ed17a3e7774b2abb3a20fab8a3e623c32f9
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2024-05-11 14:56:12 +0100

    chat box

--- resources/views/components/chat-assistant.blade.php
+++ resources/views/components/chat-assistant.blade.php
@@ -1,4 +1,16 @@
 <div class="dt_sections chat_assistant" style="margin: 14px 0 0;">
-    <h5 id="_load_more" class="empty_state">
-    </h5>
+    <div class="head d-flex justify-content-between">
+        <p class="mb-0 fw-bold">Chat Assistant AI</p>
+        <div class="d-flex">
+            <p class="mb-0 mx-3">icon</p>
+            <p class="mb-0">icon</p>
+        </div>
+    </div>
+
+    <div class="chat_container"></div>
+
+    <div class="send d-flex">
+        <textarea name="" placeholder="Type message......" id=""></textarea>
+        <button class="">Send Message</button>
+    </div>
 </div>

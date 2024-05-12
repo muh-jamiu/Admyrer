9568ff0ec1e1c0b344b3c7f4100c99647f3042ab
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2024-05-12 09:31:53 +0100

    polls

--- resources/views/pages/user_polls.blade.php
+++ resources/views/pages/user_polls.blade.php
@@ -2,7 +2,9 @@
 
 @php
 	$user = $data["user"] ?? [];
-	$liked = $data["liked"] ?? [];
+	$Allpoll = $data["Allpoll"] ?? [];
+	$Userpoll = $data["Userpoll"] ?? [];
+    $test = [1,2,3,4,5,6];
 @endphp
 
 @section('title')
@@ -13,11 +15,45 @@ Polls | Admyrer
 
 <x-main-nav :user="$user"></x-main-nav>
 
-<div class="container container-fluid container_new page-margin find_matches_cont">
+<div class="container polls container-fluid container_new page-margin find_matches_cont">
 	<div class="row r_margin">
 		{{-- <div class="col-sm-9"> --}}
 			<x-dashboard-sidebar :user="$user" :pollactive="true"></x-dashboard-sidebar>	
 		{{-- </div> --}}
+
+        <div class="col-sm-9">
+			<!-- Filters  -->
+			<div class="dt_home_filters_prnt">
+				<div class="dt_home_filters">
+					<h6><?php echo __('Vote Poll');?></h6>
+				</div>
+			</div>  
+            
+            @if (count($test) > 0)
+                <div class="row" id="liked_users_container">
+                    @foreach ($test as $key => $poll)					
+                        <div class="col-sm-4 m6 s12 matches visit likeUserrs" >
+                            <div class="card valign-wrapper" style="border: none !important"> 
+                                <div class="head">
+                                    <p class="qst fw-bold">What is your name and what do you think about our webiste?</p>
+                                </div>                              
+								<div class="card-content  p-3 w-100" >
+                                    <p class="ans"><span>A</span> Yes</p>
+                                    <p class="ans"><span>B</span> No</p>
+                                    <p class="ans"><span>C</span> Maybe</p>
+                                    <p class="ans"><span>D</span> Awesome</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            @endif
+
+			@if (count($test) == 0)
+            	<x-dashboard-empty></x-dashboard-empty> 				
+			@endif       
+    
+		</div>
 	</div>
 </div>
 

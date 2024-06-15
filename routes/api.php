1b239bd957c12029204f842f57ffa72302ebc809
muh-jamiu <ganiujamiu03@gmail.com>
2024-06-15 17:29:01 +0100

    fixes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/test',function(){
+    return response()->json("data");
 });
